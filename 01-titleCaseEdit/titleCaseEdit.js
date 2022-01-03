60f620643ee0932b32511afd806f65e5cc0bd459
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-02 19:19:47 -0500

    Q#1

---
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -0,0 +1,11 @@
+function titleCaseEdit(title) {
+  // Insert code here;
+  let str = title.toLowerCase().split(" ")
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1)
+  }
+  return str.join(" ")
+}
+
+// Do not edit this line;
+module.exports = titleCaseEdit;
