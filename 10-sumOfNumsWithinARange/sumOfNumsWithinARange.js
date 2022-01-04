ab4a473f77fbd9c8728eae353ab0fbb751a9f37a
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:33:35 -0500

    Q#11

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,11 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let e = nums.indexOf(end)
+  let s =nums.indexOf(start)
+  if (s>e) {
+    return -1
+  }
+  return e-s
 }
 
 // Do not edit this line;
