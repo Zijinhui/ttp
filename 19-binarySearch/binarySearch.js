a10a25412de08faa66cee106e01d105563a587af
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:40:03 -0500

    Q#20

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,24 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let arr = nums
+    let mid = Math.floor(arr.length/2)
+
+    if(arr.length === 1 && arr[0] != target) {
+      return false
+    }
+
+    if(arr[mid] === target) {
+      return true
+    } else if (target < arr[mid]) {
+      return this.binarySearch(arr.slice(0,mid), target)
+    } else if (target > arr[mid]) {
+      return this.binarySearch(arr.slice(mid), target)
+    }
+    
+
   }
+
 }
 
 // Do not edit this line;
