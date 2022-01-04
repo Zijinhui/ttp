01240353d71d7c092515cd749f88304478d76b8f
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:35:55 -0500

    Q$15

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let map = {}
+  for (let i = 0; i < word.length; i++) {
+    if(map[word.charAt(i)]) {
+      map[word.charAt(i)]++
+    } else {
+      map[word.charAt(i)] = 1
+    }
+  }
+  return map
 }
 
 // Do not edit this line;
