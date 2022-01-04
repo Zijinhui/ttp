e537fde2fd69c6eab9f406c6864a7e7db4c3d859
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:39:30 -0500

    Q#19

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,19 @@
 function pairSum(nums, target) {
   // Insert code here;
+    if(nums.length<=1) {
+    throw new exception()
+  }
+
+  let map = new Map()
+  for (let i=0;i<nums.length;i++) {
+    const diff=target-nums[i]
+    if(map.get(diff) != undefined) {
+      return true
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+  return false
 }
 
 // Do not edit this line;
