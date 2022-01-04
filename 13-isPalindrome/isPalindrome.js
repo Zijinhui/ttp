97324c3d2d8fb90a463a886444ffd3e3ee12e5c2
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:35:24 -0500

    Q#14

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,18 @@
 function isPalindrome(word) {
   // Insert code here;
+  if (word.length === 1) {
+    return true
+  }
+  let i = 0;
+  let j = word.length-1
+  while (i<j) {
+    if(word.charAt(i) != word.charAt(j)) {
+      return false
+    }
+    i++
+    j--
+  }
+  return true;
 }
 
 // Do not edit this line;
