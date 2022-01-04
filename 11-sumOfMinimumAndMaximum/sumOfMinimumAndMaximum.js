17028544f34dd3929dfebe2f668afc2fa06ac1c2
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:34:05 -0500

    Q#12

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,15 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let copy = nums.map(function(e){
+    return e
+  })
+
+  copy = copy.sort(function(a,b){
+      return a-b
+  })
+
+
+  return copy[0]+copy[copy.length-1]
 }
 
 // Do not edit this line;
