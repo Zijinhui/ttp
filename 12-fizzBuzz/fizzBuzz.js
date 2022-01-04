98d3d0acfae78bebbdbbc217f12d1293babc6b7b
Zijinhui Zhuang <zhuangzijinhui@gmail.com>
2022-01-03 21:34:43 -0500

    Q#13

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,20 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  let arr = []
+  
+  for (let i = start-1; i < end; i++) {
+    if ((i+1)%15 === 0) {
+      arr.push("FizzBuzz") 
+    } else if ((i+1)%5 === 0) {
+      arr.push("Buzz")
+    } else if ((i+1)%3 === 0) {
+      arr.push("Fizz")
+    } else {
+      arr.push(i+1)
+    }
+
+  }
+  return arr
 }
 
 // Do not edit this line;
